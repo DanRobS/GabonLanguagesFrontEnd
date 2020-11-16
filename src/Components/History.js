4e94920fdee52985e7bf6c5ca3e04afe8cf07444
DanRobS <roberttsoumbou@gmail.com>
2020-11-16 18:07:00 +0100

    front-end mockup final push

--- src/Components/History.js
+++ src/Components/History.js
@@ -8,12 +8,13 @@ class History extends React.Component {
   render() {
     return (
       <div className="Main_div" class="container">
-        <div className="Section_type_text" class="container">
+        <div className="Section_type_text">
           <h1 className="Section_title"> Section title </h1>
           <p className="Section_text">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
         </div>
+
         <div class="media" className="Section_type_picture">
           <img src="img_girl.jpg" width="50px" height="50px" class="mr-3"/>
           <div class="media-body">
