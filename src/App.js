2ed872425278255a7758968ac70d29c5248a9a20
DanRobS <roberttsoumbou@gmail.com>
2020-11-16 13:41:32 +0100

    adding Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import './custom.scss'
-import Welcome from './Components/Welcome'
+import Dictionary from './Components/Dictionary'
 import React from 'react'
-import ReactDOM from 'react-dom'
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Button, NavDropdown,
         Tabs, Tab  } from 'react-bootstrap'
 
@@ -14,39 +12,37 @@ class App extends React.Component {
     return (
       <div className="App">
 
-      <Navbar collapseOnSelect expand="lg" bg="dark"
-              variant="dark" className="Navbar">
-        <Navbar.Brand className="Navbar-Brand"
-            href="/">Online language translator</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link className="homeLink" href="/">Home</Nav.Link>
-          <NavDropdown title="Dictionaries" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#Nzebi">Nzebi</NavDropdown.Item>
-            <NavDropdown.Item href="#Fang">Fang</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link className="homeLink" href="/">About</Nav.Link>
-        </Nav>
-      </Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="dark"
+                variant="dark" className="Navbar">
+          <Navbar.Brand className="Navbar-Brand"
+              href="/">Online language dictionary</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link className="homeLink" href="/">Home</Nav.Link>
+            <NavDropdown title="Dictionaries" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#Nzebi">Nzebi</NavDropdown.Item>
+              <NavDropdown.Item href="#Fang">Fang</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link className="homeLink" href="/">About</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      <div className="Tabs">
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-          <Tab eventKey="dictionary" title="Dictionary">
+        <div className="Tabs">
+          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+            <Tab eventKey="dictionary" title="Dictionary">
+              <Dictionary/>
+            </Tab>
+            <Tab eventKey="history" title="History">
 
-          </Tab>
-          <Tab eventKey="history" title="History">
-
-          </Tab>
-          <Tab eventKey="language specifics"
-                title="Languague specifics">
-          </Tab>
-        </Tabs>
-      </div>
-
+            </Tab>
+            <Tab eventKey="language specifics"
+                  title="Languague specifics">
+            </Tab>
+          </Tabs>
+        </div>
 
       </div>
     );
   }
-
 }
 
 export default App;
---
+++ src/Components/Dictionary.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import '../Components_styles/Dictionary.css'
+import '../custom.scss'
+
+class Dictionary extends React.Component {
+
+  render() {
+    return (
+      <div className="Main_div">
+        <div className="Form_div">
+          <Form inline className="Input_form">
+            <Form.Label htmlFor="inlineFormInputName2" srOnly>
+              Name
+            </Form.Label>
+            <Form.Control
+              className="mb-2 mr-sm-2"
+              id="inlineFormInputName2"
+              placeholder="Type a word"
+            />
+            <Button type="submit" className="mb-2">
+              Translate
+            </Button>
+          </Form>
+        </div>
+        <div className="Response_div">
+          <h2 className="Response_word"> Response word </h2>
+          <h4 className="Response_example"> this is an example (inzebi) </h4>
+          <h4 className="Response_translation"> this is an example translation in french </h4>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dictionary;
