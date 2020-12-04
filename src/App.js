fae488e74e01e98b99c0afd9aa0c0bd4b16e0197
DanRobS <roberttsoumbou@gmail.com>
2020-12-04 19:02:21 +0100

    fixing router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './custom.scss'
 import Dictionary from './Components/Dictionary'
 import History from './Components/History'
 import LanguageSpecifics from './Components/LanguageSpecifics'
-import About from './Components/About'
 import React from 'react'
 import { Navbar, Nav, NavDropdown,
         Tabs, Tab  } from 'react-bootstrap'
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Link,
+          Switch } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -22,35 +22,35 @@ class App extends React.Component {
                 href="/">Online language dictionary</Navbar.Brand>
             <Nav className="mr-auto">
               <Nav.Item>
-                <Nav.Link className="homeLink" as={Link} to="/">Home</Nav.Link>
+                <Nav.Link className="homeLink" href="/">Home</Nav.Link>
               </Nav.Item>
               <NavDropdown title="Dictionaries" id="basic-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/Dictionaries/Nzebi">Nzebi</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/Dictionaries/Fang">Fang</NavDropdown.Item>
               </NavDropdown>
               <Nav.Item>
-                <Nav.Link className="homeLink" as={Link} to="/about">About</Nav.Link>
+                <Nav.Link className="homeLink" href="/about">About</Nav.Link>
               </Nav.Item>
             </Nav>
           </Navbar>
-        </Router>
-
 
         <div className="Tabs">
+        <Switch>
           <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
             <Tab eventKey="dictionary" title="Dictionary">
-              <Dictionary/>
+              <Route exact path='/Dictionaries/Nzebi' component={Dictionary}></Route>
             </Tab>
             <Tab eventKey="history" title="History">
-              <History/>
+              <Route exact path='/Dictionaries/Nzebi' component={History}></Route>
             </Tab>
             <Tab eventKey="language specifics"
                   title="Languague specifics">
-              <LanguageSpecifics/>
+              <Route exact path='/Dictionaries/Nzebi' component={LanguageSpecifics}></Route>
             </Tab>
           </Tabs>
+        </Switch>
         </div>
-
+        </Router>
       </div>
     );
   }
